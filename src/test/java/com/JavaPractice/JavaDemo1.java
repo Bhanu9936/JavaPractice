6bb6e8018d922b18c08f5f67dc4fb0026aec926c
Bhanu9936 <71899647+Bhanu9936@users.noreply.github.com>
2022-07-18 01:04:35 +0530

    fetch data

--- src/test/java/com/JavaPractice/JavaDemo1.java
+++ src/test/java/com/JavaPractice/JavaDemo1.java
@@ -5,6 +5,7 @@ public class JavaDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add to index");
+		System.out.println("fetch the data");
 
 	}
 
