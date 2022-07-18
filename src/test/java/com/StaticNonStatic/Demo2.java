e7296570148e10ceffdb4d84589328db6a927a9a
BhanuB_9936 <bhanu45369@gmail.com>
2022-07-18 11:47:58 +0530

    Demo2 file is updated

--- src/test/java/com/StaticNonStatic/Demo2.java
+++ src/test/java/com/StaticNonStatic/Demo2.java
@@ -5,14 +5,25 @@ public class Demo2 {
 	int a = 10;
 	String name = "Bhanu";
 	
+	
+	public void check() {
+		Demo2 demo = new Demo2();
+		
+		System.out.println("Call Non static variables in Non Static method using creation of object");
+		System.out.println(demo.a);
+		System.out.println(demo.name);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Demo2 demo = new Demo2();
 	
-		System.out.println("Call Non static methods in Static method using creation of object");
+		System.out.println("Call Non static variables in Static method using creation of object");
 		System.out.println(demo.a);
 		System.out.println(demo.name);
+		
+		demo.check();
 		
 		
 
